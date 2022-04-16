92d5de950fec0bd325e11890898a68f31b576a0a
charles okunzo <okunzopeter01@gmail.com>
2022-04-17 00:26:34 +0300

    style:users component-created navbarcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitUsersComponent } from './git-users/git-users.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitUsersComponent
+    GitUsersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
