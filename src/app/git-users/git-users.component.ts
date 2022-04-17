e4d0ea9b2d9503700b91c518225e667ffc3c8f5b
charles okunzo <okunzopeter01@gmail.com>
2022-04-17 15:41:28 +0300

    style:modify the nav bar by adding dropdown on menu

--- src/app/git-users/git-users.component.ts
+++ src/app/git-users/git-users.component.ts
@@ -12,15 +12,15 @@ export class GitUsersComponent implements OnInit {
 
   userDetails:any= [];
 
+  showResults=false;
+
   constructor(private githubApi:GitHubAPIService) { }
 
   collectUserName(){
+    this.showResults=true;
       this.githubApi.getGitHubUserDetails(this.username).then((resp)=>{
         this.userDetails= resp
       }, err=> alert('User Not Found'))
-
-      console.log(this.username)
-    return false;
   }
 
   ngOnInit(): void {
