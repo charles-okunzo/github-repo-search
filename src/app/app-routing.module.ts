e8d5f577455f6c8a3ddcf020beeb193d18e6d6d2
charles okunzo <okunzopeter01@gmail.com>
2022-04-17 20:03:08 +0300

    feature:routing-add routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitReposComponent } from './git-repos/git-repos.component';
+import { GitUsersComponent } from './git-users/git-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'github-users', component:GitUsersComponent},
+  {path:'github-repositories', component:GitReposComponent},
+  {path:'', redirectTo:'github-users', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
