5aa4b15ec97ee46ffebbc2857709780396412c47
charles okunzo <okunzopeter01@gmail.com>
2022-04-18 22:26:41 +0300

    Auto-generated commit [ci skip]

--- src/app/repos.ts
+++
@@ -1,8 +0,0 @@
-export class Repos {
- constructor(
-   public name:string,
-   public description:string,
-   public updated_at:Date,
-   public html_url:string
- ){ }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  GitHubAPIUrl: 'https://api.github.com',
-  gitPAT: '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',
-};
