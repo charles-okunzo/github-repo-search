69428401a2617bba3df92428215c579f8d199f09
charles okunzo <okunzopeter01@gmail.com>
2022-04-18 16:35:46 +0300

    style:404-add routing for 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { GitReposComponent } from './git-repos/git-repos.component';
 import { GitUsersComponent } from './git-users/git-users.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:'github-users', component:GitUsersComponent},
   {path:'github-repositories', component:GitReposComponent},
   {path:'home-page', component:HomePageComponent},
-  {path:'', redirectTo:'github-users', pathMatch:'full'}
+  {path:'', redirectTo:'github-users', pathMatch:'full'},
+  {path:'**', component:NotFoundComponent}
 ];
 
 @NgModule({
